7fabb20c45869237f6b63b48e2f465188ad4419b
gda-conc <gabrieldaconceicaodeoliveira@gmail.com>
2025-07-16 20:23:31 -0300

    refactor(norminette): :recycle: main to norminette
    I1m putting the functions to norminnete, the main is ok

--- src/mlx_usage.c
+++ src/mlx_usage.c
@@ -1,12 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_usage.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gda-conc <gda-conc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 16:04:00 by gda-conc          #+#    #+#             */
+/*   Updated: 2025/07/16 16:06:38 by gda-conc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rt.h"
 
-void	init_mlx(t_mlx *mlx, int width, int height)
+void	init_mlx(t_rt *rt)
 {
+	t_mlx *mlx;
+
+	mlx = malloc(sizeof(t_mlx));
+	if (!mlx)
+		return;
 	mlx->mlx_ptr = mlx_init();
-	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr,width,height, "janelaaaaa");
-	mlx->img = mlx_new_image(mlx->mlx_ptr, width, height);
-	mlx->addr = mlx_get_data_addr(mlx->img,&mlx->bits_per_pixel,\
-		&mlx->line_length,&mlx->endian);
+	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr,\
+		rt->image_width, rt->image_height, "janelaaaaa");
+	mlx->img = mlx_new_image(mlx->mlx_ptr, rt->image_width,\
+		 rt->image_height);
+	mlx->addr = mlx_get_data_addr(mlx->img, &mlx->bits_per_pixel,
+		&mlx->line_length, &mlx->endian);
+	rt->mlx = mlx;
 }
 
 int	destroy(t_mlx *mlx)
